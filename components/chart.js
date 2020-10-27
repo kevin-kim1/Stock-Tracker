6debd67663c400108be428b291cd32ad14a048de
Kevin Kim <kevinnhkim@live.com>
2020-10-27 15:31:27 -0700

    created symbol variable to allow the select element to pick which stock data is displayed on chart

--- components/chart.js
+++ components/chart.js
@@ -1,9 +1,10 @@
 loadingChart();
 function loadingChart() {
+    let symbol = document.getElementById('pairs').value;
     new TradingView.widget({
       width: 980,
       height: 610,
-      symbol: "NASDAQ:AAPL",
+      symbol: symbol,
       interval: "D",
       timezone: "Etc/UTC",
       theme: "light",
