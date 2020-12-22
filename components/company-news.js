daff184914552d316d5cade66b80eb871463c1dc
Kevin Kim <kevinnhkim@live.com>
2020-12-22 15:47:34 -0800

    added click handler to button to get the input value from form element for string symbol var to put into fetch get request

--- components/company-news.js
+++ components/company-news.js
@@ -1,12 +1,26 @@
-loadNews();
 function loadNews() {
   const currentDate = new Date();
   const dd = String(currentDate.getDate()).padStart(2, '0');
   const mm = String(currentDate.getMonth() + 1).padStart(2, '0');
   const year = currentDate.getFullYear();
   const date = year + '-' + mm + '-' + dd;
-  const newsData = fetch('https://finnhub.io/api/v1/company-news?symbol=AAPL&from=' + date +'&to='+ date +'&token=btp4fp748v6pbh1ev3a0')
-    .then(res => res.json())
-    .then(data => console.log(data));
-  console.log(newsData);
+  document.getElementById('news-symbol-button').addEventListener('click', () => {
+    const inputText = document.getElementById("news-symbol-text");
+    const symbol = inputText.value;
+    const newsData = fetch(
+      "https://finnhub.io/api/v1/company-news?symbol="+ symbol +"&from=" +
+        date +
+        "&to=" +
+        date +
+        "&token=btp4fp748v6pbh1ev3a0"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        // document.getElementById('headline').textContent = data[0].headline;
+      });
+    console.log(newsData);
+    console.log(symbol);
+  })
 }
+loadNews();
